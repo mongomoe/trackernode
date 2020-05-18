40a6f607fcb2f42c0d788ba414d54469dcc28346
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-05-18 11:40:44 -0400

    update full app

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const Realm = require("realm");
 const ObjectId = require("bson").ObjectID;
+const {
+  Stitch,
+  AnonymousCredential,
+  UserPasswordCredential,
+  UserPasswordAuthProviderClient,
+} = require("mongodb-stitch-server-sdk");
+
+async function login() {
+  const app = Stitch.initializeDefaultAppClient("rntut-hefbl");
+  // const emailPasswordClient = Stitch.defaultAppClient.auth.getProviderClient(
+  //   UserPasswordAuthProviderClient.factory
+  // );
+
+  // emailPasswordClient
+  //   .registerWithEmail("moechug@mailinator.com", "Moechug123!")
+  //   .then(() => console.log("Successfully sent account confirmation email!"))
+  //   .catch((err) => console.error("Error registering new user:", err));
+
+  const credential = new UserPasswordCredential(
+    "moechug@mailinator.com",
+    "Moechug123!"
+  );
+
+  return await app.auth
+    .loginWithCredential(credential)
+    // Returns a promise that resolves to the authenticated user
+    .then(async (authedUser) => {
+      console.log(`successfully logged in with id: ${authedUser.id}`);
+      return authedUser.id;
+    })
+    .catch((err) => console.error(`login failed with error: ${err}`));
+}
+// login();
+
 async function run() {
   const UserSchema = {
     name: "User",
@@ -10,7 +44,6 @@ async function run() {
       project: { type: "Project" },
     },
   };
-
   const ProjectSchema = {
     name: "Project",
     primaryKey: "_id",
@@ -31,7 +64,6 @@ async function run() {
       comments: "string",
     },
   };
-
   const realmConfig = {
     schema: [UserSchema, ProjectSchema, TaskSchema],
     // sync: {
@@ -39,14 +71,13 @@ async function run() {
     //   partitionValue: '"LoLo"',
     // },
   };
-
   let realm = await Realm.open(realmConfig);
-
-  console.log("realm exist", realm);
+  const userID = await login();
+  console.log("user id ->", userID);
 
   realm.write(() => {
     const MoeUser = realm.create("User", {
-      _id: new ObjectId(),
+      _id: new ObjectId(userID),
       name: "Moe",
     });
     const MoeProject = realm.create("Project", {
@@ -54,9 +85,7 @@ async function run() {
       name: "Tracker",
     });
     MoeUser.project = MoeProject;
-
     const taskList = MoeProject.tasks;
-
     const GroceryTask = realm.create("Task", {
       _id: new ObjectId(),
       title: "Buy Groceries",
@@ -64,7 +93,6 @@ async function run() {
       description: "go to the store to buy milk, eggs, and bread",
       comments: "check expiration date",
     });
-
     const ExerciseTask = realm.create("Task", {
       _id: new ObjectId(),
       title: "Exercise",
@@ -72,13 +100,10 @@ async function run() {
       description: "go to the gym",
       comments: "leg day",
     });
-
     taskList.push(GroceryTask);
-
     taskList.push(ExerciseTask);
-
+    console.log("user details", MoeUser);
     console.log("original exercise task status", ExerciseTask.status);
-
     // console.log("project including all tasks", MoeUser.project);
   });
 
@@ -100,11 +125,8 @@ async function run() {
     let tasks = realm.objects("Task");
     let ExerciseTask = tasks.filtered('title = "Exercise"');
     realm.delete(ExerciseTask);
-
     // console.log("remaining tasks", tasks);
   });
-
-  realm.close();
 }
 
 run();
