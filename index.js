dbfb1d4b6600b45cabbd57a9e2c72cc9af85bb0e
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-05-18 13:29:25 -0400

    update repo w working index for crud local db

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const {
   UserPasswordAuthProviderClient,
 } = require("mongodb-stitch-server-sdk");
 
+// MOCK LOGIN FUNCTION
 async function login() {
   const app = Stitch.initializeDefaultAppClient("rntut-hefbl");
+
+  /* REGISTER
   // const emailPasswordClient = Stitch.defaultAppClient.auth.getProviderClient(
   //   UserPasswordAuthProviderClient.factory
   // );
@@ -17,6 +20,7 @@ async function login() {
   //   .registerWithEmail("moechug@mailinator.com", "Moechug123!")
   //   .then(() => console.log("Successfully sent account confirmation email!"))
   //   .catch((err) => console.error("Error registering new user:", err));
+  */
 
   const credential = new UserPasswordCredential(
     "moechug@mailinator.com",
@@ -28,13 +32,15 @@ async function login() {
     // Returns a promise that resolves to the authenticated user
     .then(async (authedUser) => {
       console.log(`successfully logged in with id: ${authedUser.id}`);
+      console.log(authedUser.id);
       return authedUser.id;
     })
     .catch((err) => console.error(`login failed with error: ${err}`));
 }
-// login();
 
 async function run() {
+  // CREATE SCHEMAS
+
   const UserSchema = {
     name: "User",
     primaryKey: "_id",
@@ -66,15 +72,15 @@ async function run() {
   };
   const realmConfig = {
     schema: [UserSchema, ProjectSchema, TaskSchema],
-    // sync: {
+    // sync: { // stitch stuff needs to be working for user to work with stitch?
     //   user: user,
-    //   partitionValue: '"LoLo"',
+    //   partitionValue: "MyProject",
     // },
   };
   let realm = await Realm.open(realmConfig);
   const userID = await login();
-  console.log("user id ->", userID);
 
+  // CREATE USER, PROJECT, AND MULTIPLE TASKS
   realm.write(() => {
     const MoeUser = realm.create("User", {
       _id: new ObjectId(userID),
@@ -102,11 +108,11 @@ async function run() {
     });
     taskList.push(GroceryTask);
     taskList.push(ExerciseTask);
-    console.log("user details", MoeUser);
     console.log("original exercise task status", ExerciseTask.status);
     // console.log("project including all tasks", MoeUser.project);
   });
 
+  // UPDATE TASK
   realm.write(() => {
     let tasks = realm.objects("Task");
     let ExerciseTask = tasks.filtered('title = "Exercise"');
@@ -114,6 +120,7 @@ async function run() {
     console.log("updated exercise task status", ExerciseTask.status);
   });
 
+  // DELETE TASK
   realm.write(() => {
     let tasks = realm.objects("Task");
     let ExerciseTask = tasks.filtered('title = "Exercise"');
